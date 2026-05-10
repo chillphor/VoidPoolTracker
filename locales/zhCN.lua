b4299be5c70ecc62cee12555f343944e6e825078
chillphor <56460965+chillphor@users.noreply.github.com>
2026-05-10 18:28:37 +0800

    Add Chinese (Simplified) localization file

---
+++ locales/zhCN.lua
@@ -0,0 +1,49 @@
+local L = {}
+
+-- 面板标题
+L["PANEL_TITLE"] = "虚空宝箱全账号查询"
+L["LOOT_REMAINING"] = "剩余"
+
+-- 按钮
+L["BUTTON_SYNC"] = "同步"
+L["BUTTON_ANNOUNCE"] = "发送"
+L["BUTTON_SELECT_CHARACTER"] = "选择查看角色"
+L["BUTTON_CLOSE"] = "关闭"
+
+-- 副本名称
+L["DUNGEON_ACADEMY"] = "艾杰斯亚学院"
+L["DUNGEON_PLATFORM"] = "魔导师平台"
+L["DUNGEON_NODE"] = "节点希纳斯"
+L["DUNGEON_MINES"] = "萨隆矿坑"
+L["DUNGEON_CONCLAVE"] = "执政团之座"
+L["DUNGEON_PEAK"] = "通天峰"
+L["DUNGEON_WINDRUNNER"] = "风行者之塔"
+L["DUNGEON_MESARA"] = "迈萨拉洞窟"
+
+-- 系统消息
+L["MSG_SYNC_SUCCESS"] = "[虚空查询]: %s 数据同步成功"
+L["MSG_SYSTEM_LOADING"] = "[虚空查询]: 系统正在加载，请稍后再试"
+L["MSG_DATA_NOT_READY"] = "[虚空查询]: 数据未就绪，请稍后再试"
+L["MSG_NO_GROUP"] = "[虚空查询]: 未在队伍中，无法发送通报"
+L["MSG_NO_DATA"] = "[虚空查询]: 数据库中暂无记录，无法通报"
+L["MSG_LOADING"] = "加载中...请刷新"
+L["MSG_NO_RECORDS"] = "数据库中暂无该角色记录，请点击右上角"同步当前数据""
+
+-- 聊天通报
+L["ANNOUNCE_HEADER"] = "=== 剩余奖池-%s ==="
+L["ANNOUNCE_CHARACTER"] = "角色: %s"
+
+-- 字体颜色
+L["YOUR_CHARACTER"] = "|cff00ff00"  -- 绿色
+L["OTHER_CHARACTER"] = "|cff00ccff" -- 青色
+L["GRAY_TEXT"] = "|cff888888"       -- 灰色
+L["RED_TEXT"] = "|cffff0000"        -- 红色
+L["PURPLE_TEXT"] = "|cffa335ee"     -- 紫色
+L["CYAN_TEXT"] = "|cff00ffff"       -- 青色
+
+-- 时间格式
+L["TIME_HOUR"] = "%d小时"
+L["TIME_MINUTE"] = "%d分"
+L["TIME_SECOND"] = "%d秒"
+
+return L
